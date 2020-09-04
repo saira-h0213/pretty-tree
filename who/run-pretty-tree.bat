11ab0eb11ab0f840d49a0587195f9ffd1201fc00
saira-h0213 <37966631+saira-h0213@users.noreply.github.com>
2020-09-04 13:01:30 +0100

    Modify the batch file to the right source location

--- who/run-pretty-tree.bat
+++ who/run-pretty-tree.bat
@@ -5,7 +5,7 @@ ECHO Running Pretty Tree...
 START c:\progra~1\r\R-4.0.2\bin\Rscript.exe -e "source('ppi.R')"
 ECHO Packages Installed Successfully
 
-START c:\progra~1\r\R-4.0.2\bin\Rscript.exe -e "library(devtools); source_url('https://raw.githubusercontent.com/saira-h0213/pretty-tree/master/tgenerator.R')"
+START c:\progra~1\r\R-4.0.2\bin\Rscript.exe -e "library(devtools); source_url('https://raw.githubusercontent.com/saira-h0213/pretty-tree/master/src/tgenerator.R')"
 
 PAUSE
 cmd /k
